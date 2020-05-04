b07b705c25358913e89f5e25e0a427471fa84bce
Andrew <andrea.awesome.ua@gmail.com>
2020-05-04 16:17:40 +0300

    Initial commit

---
+++ app/src/main/java/shkuratov/andrew/webandmultimedia/MainActivity.java
@@ -0,0 +1,19 @@
+package shkuratov.andrew.webandmultimedia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
